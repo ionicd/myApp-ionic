65d227373b52ccac2cd5e6c66ab81a1561db0724
SAURAV KC <saurav.kc14@myhunter.cuny.edu>
2021-06-29 13:27:37 -0400

    Added http module, getters of REST api

--- src/app/activity.service.ts
+++ src/app/activity.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http'
 @Injectable({
   providedIn: 'root'
 })
 export class ActivityService {
 
-  constructor() { }
+  constructor(private _httpClient: HttpClient) { }
 
-  getActivity(){
-
+  // returns specific activity
+  getActivity(activityID: string){
+    return this._httpClient.get(API + "/id" +
+       activityID);
   }
   
+  // returns an observable with some data in it
   getAllActivities(){
-    
+    return this._httpClient.get(API);
   }
 }
+
+const API = "https://orangevalleycaa.org/api/videos"
