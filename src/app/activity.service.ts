dad5789ea55a545e38a081e1cb4114f19deb66bb
SAURAV KC <saurav.kc14@myhunter.cuny.edu>
2021-06-29 14:21:24 -0400

    Completed home page

--- src/app/activity.service.ts
+++ src/app/activity.service.ts
@@ -1,21 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Activity } from './types';
 @Injectable({
   providedIn: 'root'
 })
+
 export class ActivityService {
 
   constructor(private _httpClient: HttpClient) { }
 
   // returns specific activity
-  getActivity(activityID: string){
-    return this._httpClient.get(API + "/id/" +
+  getActivity(activityID: string): Observable<Activity> {
+    return this._httpClient.get<Activity>(API + "/id/" +
        activityID);
   }
   
   // returns an observable with some data in it
-  getAllActivities(){
-    return this._httpClient.get(API);
+  getAllActivities(): Observable<Activity[]>{
+    return this._httpClient.get<Activity[]>(API);
   }
 }
 
